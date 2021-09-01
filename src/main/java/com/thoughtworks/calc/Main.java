0d50d6aaf56f24b3d5baaf2c228974d2eccffa42
A. Matías Quezada <matias.quezada_ext@autoscout24.com>
2021-09-01 17:56:40 +0200

    print result when only one integer is provided

--- src/main/java/com/thoughtworks/calc/Main.java
+++ src/main/java/com/thoughtworks/calc/Main.java
@@ -3,7 +3,21 @@ package com.thoughtworks.calc;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("some help message");
+    if (args.length == 1) {
+      try {
+        int input = Integer.parseInt(args[0]);
+        System.out.println(input);
+        return;
+      } catch (NumberFormatException ex) {
+        showHelpMessage();
+        return;
+      }
+    }
 
+    showHelpMessage();
+  }
+
+  private static void showHelpMessage() {
+    System.out.println("some help message");
   }
 }
