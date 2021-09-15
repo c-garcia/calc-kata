001a5e783066ed2bd2d8ec93e1ae08443fc9a73a
Marcos Vinícius da Silva <marcos.silva@thoughtworks.com>
2021-09-15 17:57:48 +0200

    prepare subtraction by keeping compatible with addition

--- src/main/java/com/thoughtworks/calc/Main.java
+++ src/main/java/com/thoughtworks/calc/Main.java
@@ -5,15 +5,23 @@ public class Main {
   public static void main(String[] args) {
     if (args.length == 1) {
       try {
-        String[] tokens = args[0].split("\\+");
+        String operator = "\\+|-";
+        String real_operator = "\\+";
+        String[] tokens = args[0].split(operator);
         //No plus sign
+
         if(tokens.length == 1) {
           System.out.println(Integer.parseInt(tokens[0]));
           return;
         }
+
         int a = Integer.parseInt(tokens[0]);
         int b = Integer.parseInt(tokens[1]);
-        System.out.println(a + b);
+
+        if (real_operator.equals("\\+"))  {
+          System.out.println(a + b);
+        }
+
         return;
       } catch (NumberFormatException ex) {
         showHelpMessage();
